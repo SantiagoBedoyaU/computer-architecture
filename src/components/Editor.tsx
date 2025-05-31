52fdce775ce615c3b0388157fb083eddb0929ed6
SantiagoBedoyaU <santiago.bedoya35419@ucaldas.edu.co>
2025-05-30 22:07:05 -0500

    Fixing editor validations

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -17,251 +17,278 @@ export const CodeEditor = () => {
   const loadProgram = () => {
     handleClear();
     const code: string = editorRef.current.getValue();
-    const lines = code.split("\n");
-    lines
-      .filter((line) => line.trim().length > 0)
-      .forEach((line) => {
-        const [codop, parameters] = line.trim().toUpperCase().split(" ");
-        switch (codop) {
-          case "MOV":
-            {
-              const operands = parameters.split(",");
-              if (operands.length < 2) {
+    let lines = code.split("\n");
+    lines = lines.filter((line) => line.trim().length > 0)
+    let isSuccess = true;
+    for (const line of lines) {
+      const stms = line.trim().toUpperCase().split(" ");
+      if (stms.length < 2) {
+        Alert({
+          text: `Operación incorrecta: "${line}"`,
+        })
+        isSuccess = false;
+        break;
+      }
+      const [codop, parameters] = stms
+      switch (codop) {
+        case "MOV":
+          {
+            const operands = parameters.split(",");
+            if (operands.length < 2) {
+              Alert({
+                text: "Para el CODOP MOV se necesitan 2 operandos",
+              });
+              isSuccess = false;
+              break;
+            }
+            const register = operands[0];
+            const value = parseInt(operands[1]);
+            if (!registerOptions.includes(register)) {
+              Alert({
+                text: "No se puede utilizar este registro",
+              });
+              isSuccess = false;
+              break;
+            }
+            if (value > 127 || value < -127) {
+              Alert({
+                text: "El valor no puede ser mayor a 127 ni menor a -127",
+              });
+              isSuccess = false;
+            }
+            createItem({
+              codop,
+              operand1: register,
+              operand2: value.toString(),
+            });
+          }
+          break;
+        case "ADD":
+          {
+            const operands = parameters.split(",");
+            if (operands.length < 2) {
+              Alert({
+                text: "Para el CODOP ADD se necesitan 2 operandos",
+              });
+              isSuccess = false;
+              break;
+            }
+            const register = operands[0];
+            const value = operands[1];
+            if (!registerOptions.includes(register)) {
+              Alert({
+                text: "No se puede utilizar este registro" + ": " + value,
+              });
+              isSuccess = false;
+              break;
+            }
+            const nValue = parseInt(value);
+            if (Number.isNaN(nValue)) {
+              // it's a register
+              if (!registerOptions.includes(value)) {
                 Alert({
-                  text: "Para el CODOP MOV se necesitan 2 operandos",
+                  text: "No se puede utilizar este registro" + ": " + value,
                 });
-                break;
-              }
-              const register = operands[0];
-              const value = parseInt(operands[1]);
-              if (!registerOptions.includes(register)) {
-                Alert({
-                  text: "No se puede utilizar este registro",
-                });
-                break;
-              }
-              if (value > 127 || value < -127) {
-                Alert({
-                  text: "El valor no puede ser mayor a 127 ni menor a -127",
-                });
+                isSuccess = false;
               }
               createItem({
                 codop,
                 operand1: register,
-                operand2: value.toString(),
+                operand2: value,
+                type2: "REGISTER",
+              });
+            } else {
+              // it's a number
+              createItem({
+                codop,
+                operand1: register,
+                operand2: nValue.toString(),
               });
             }
-            break;
-          case "ADD":
-            {
-              const operands = parameters.split(",");
-              if (operands.length < 2) {
-                Alert({
-                  text: "Para el CODOP ADD se necesitan 2 operandos",
-                });
-                break;
-              }
-              const register = operands[0];
-              const value = operands[1];
-              if (!registerOptions.includes(register)) {
+          }
+          break;
+        case "DEC":
+          {
+            const operands = parameters.split(",");
+            if (operands.length < 2) {
+              Alert({
+                text: "Para el CODOP ADD se necesitan 2 operandos",
+              });
+              isSuccess = false;
+              break;
+            }
+            const register = operands[0];
+            const value = operands[1];
+            if (!registerOptions.includes(register)) {
+              Alert({
+                text: "No se puede utilizar este registro",
+              });
+              isSuccess = false;
+              break;
+            }
+            const nValue = parseInt(value);
+            if (Number.isNaN(nValue)) {
+              // it's a register
+              if (!registerOptions.includes(value)) {
                 Alert({
                   text: "No se puede utilizar este registro" + ": " + value,
                 });
-                break;
+                isSuccess = false;
               }
-              const nValue = parseInt(value);
-              if (Number.isNaN(nValue)) {
-                // it's a register
-                if (!registerOptions.includes(value)) {
-                  Alert({
-                    text: "No se puede utilizar este registro" + ": " + value,
-                  });
-                }
-                createItem({
-                  codop,
-                  operand1: register,
-                  operand2: value,
-                  type2: "REGISTER",
-                });
-              } else {
-                // it's a number
-                createItem({
-                  codop,
-                  operand1: register,
-                  operand2: nValue.toString(),
-                });
-              }
-            }
-            break;
-          case "DEC":
-            {
-              const operands = parameters.split(",");
-              if (operands.length < 2) {
-                Alert({
-                  text: "Para el CODOP ADD se necesitan 2 operandos",
-                });
-                break;
-              }
-              const register = operands[0];
-              const value = operands[1];
-              if (!registerOptions.includes(register)) {
-                Alert({
-                  text: "No se puede utilizar este registro",
-                });
-                break;
-              }
-              const nValue = parseInt(value);
-              if (Number.isNaN(nValue)) {
-                // it's a register
-                if (!registerOptions.includes(value)) {
-                  Alert({
-                    text: "No se puede utilizar este registro" + ": " + value,
-                  });
-                }
-                createItem({
-                  codop,
-                  operand1: register,
-                  operand2: value,
-                  type2: "REGISTER",
-                });
-              } else {
-                // it's a number
-                createItem({
-                  codop,
-                  operand1: register,
-                  operand2: nValue.toString(),
-                });
-              }
-            }
-            break;
-          case "DIV":
-            {
-              const operands = parameters.split(",");
-              if (operands.length < 2) {
-                Alert({
-                  text: "Para el CODOP ADD se necesitan 2 operandos",
-                });
-                break;
-              }
-              const register = operands[0];
-              const value = operands[1];
-              if (!registerOptions.includes(register)) {
-                Alert({
-                  text: "No se puede utilizar este registro",
-                });
-                break;
-              }
-              const nValue = parseInt(value);
-              if (Number.isNaN(nValue)) {
-                // it's a register
-                if (!registerOptions.includes(value)) {
-                  Alert({
-                    text: "No se puede utilizar este registro" + ": " + value,
-                  });
-                }
-                createItem({
-                  codop,
-                  operand1: register,
-                  operand2: value,
-                  type2: "REGISTER",
-                });
-              } else {
-                // it's a number
-                createItem({
-                  codop,
-                  operand1: register,
-                  operand2: nValue.toString(),
-                });
-              }
-            }
-            break;
-          case "MUL":
-            {
-              const operands = parameters.split(",");
-              if (operands.length < 2) {
-                Alert({
-                  text: "Para el CODOP ADD se necesitan 2 operandos",
-                });
-                break;
-              }
-              const register = operands[0];
-              const value = operands[1];
-              if (!registerOptions.includes(register)) {
-                Alert({
-                  text: "No se puede utilizar este registro",
-                });
-                break;
-              }
-              const nValue = parseInt(value);
-              if (Number.isNaN(nValue)) {
-                // it's a register
-                if (!registerOptions.includes(value)) {
-                  Alert({
-                    text: "No se puede utilizar este registro" + ": " + value,
-                  });
-                }
-                createItem({
-                  codop,
-                  operand1: register,
-                  operand2: value,
-                  type2: "REGISTER",
-                });
-              } else {
-                // it's a number
-                createItem({
-                  codop,
-                  operand1: register,
-                  operand2: nValue.toString(),
-                });
-              }
-            }
-            break;
-          case "JMP":
-            {
-              const operands = parameters.split(",");
-              if (operands.length < 2) {
-                Alert({
-                  text: "Para el CODOP JMP se necesita 1 operando",
-                });
-                break;
-              }
-              const value = operands[0];
-              createItem({ codop, operand1: value });
-            }
-            break;
-          case "JNZ":
-            createItem({ codop, type1: "FUNCTION", operand1: "function" });
-            break;
-          case "JZ":
-            createItem({ codop, type1: "FUNCTION", operand1: "function" });
-            break;
-          case "LOAD":
-            {
               createItem({
                 codop,
-                type1: "REGISTER",
-                operand1: "AL",
-                type2: "NUMBER",
-                operand2: "0",
+                operand1: register,
+                operand2: value,
+                type2: "REGISTER",
+              });
+            } else {
+              // it's a number
+              createItem({
+                codop,
+                operand1: register,
+                operand2: nValue.toString(),
               });
             }
-            break;
-          case "STORE":
-            createItem({ codop, type1: "NUMBER", operand1: "0" });
-            break;
-          default:
-            Alert({
-              text: "CODOP no soportado",
+          }
+          break;
+        case "DIV":
+          {
+            const operands = parameters.split(",");
+            if (operands.length < 2) {
+              Alert({
+                text: "Para el CODOP ADD se necesitan 2 operandos",
+              });
+              isSuccess = false;
+              break;
+            }
+            const register = operands[0];
+            const value = operands[1];
+            if (!registerOptions.includes(register)) {
+              Alert({
+                text: "No se puede utilizar este registro",
+              });
+              isSuccess = false;
+              break;
+            }
+            const nValue = parseInt(value);
+            if (Number.isNaN(nValue)) {
+              // it's a register
+              if (!registerOptions.includes(value)) {
+                Alert({
+                  text: "No se puede utilizar este registro" + ": " + value,
+                });
+                isSuccess = false;
+              }
+              createItem({
+                codop,
+                operand1: register,
+                operand2: value,
+                type2: "REGISTER",
+              });
+            } else {
+              // it's a number
+              createItem({
+                codop,
+                operand1: register,
+                operand2: nValue.toString(),
+              });
+            }
+          }
+          break;
+        case "MUL":
+          {
+            const operands = parameters.split(",");
+            if (operands.length < 2) {
+              Alert({
+                text: "Para el CODOP ADD se necesitan 2 operandos",
+              });
+              isSuccess = false;
+              break;
+            }
+            const register = operands[0];
+            const value = operands[1];
+            if (!registerOptions.includes(register)) {
+              Alert({
+                text: "No se puede utilizar este registro",
+              });
+              isSuccess = false;
+              break;
+            }
+            const nValue = parseInt(value);
+            if (Number.isNaN(nValue)) {
+              // it's a register
+              if (!registerOptions.includes(value)) {
+                Alert({
+                  text: "No se puede utilizar este registro" + ": " + value,
+                });
+                isSuccess = false;
+                break;
+              }
+              createItem({
+                codop,
+                operand1: register,
+                operand2: value,
+                type2: "REGISTER",
+              });
+            } else {
+              // it's a number
+              createItem({
+                codop,
+                operand1: register,
+                operand2: nValue.toString(),
+              });
+            }
+          }
+          break;
+        case "JMP":
+          {
+            const operands = parameters.split(",");
+            if (operands.length < 2) {
+              Alert({
+                text: "Para el CODOP JMP se necesita 1 operando",
+              });
+              isSuccess = false;
+              break;
+            }
+            const value = operands[0];
+            createItem({ codop, operand1: value });
+          }
+          break;
+        case "JNZ":
+          createItem({ codop, type1: "FUNCTION", operand1: "function" });
+          break;
+        case "JZ":
+          createItem({ codop, type1: "FUNCTION", operand1: "function" });
+          break;
+        case "LOAD":
+          {
+            createItem({
+              codop,
+              type1: "REGISTER",
+              operand1: "AL",
+              type2: "NUMBER",
+              operand2: "0",
             });
-            break;
-        }
-      });
+          }
+          break;
+        case "STORE":
+          createItem({ codop, type1: "NUMBER", operand1: "0" });
+          break;
+        default:
+          Alert({
+            text: "CODOP no soportado",
+          });
+          break;
+      }
+    }
 
-    Alert({
-      text: "Programa cargado correctamente",
-      icon: "success",
-      title: "Programa cargado",
-    });
+    if (isSuccess) {
+      Alert({
+        text: "Programa cargado correctamente",
+        icon: "success",
+        title: "Programa cargado",
+      });
+    }
   };
 
   // const handleCreate = (codop: keyof typeof CODOPS) => {
